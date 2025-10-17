1b9fc34ad345c468801dafc4738f3d2a675d2fb3
Paul Novack <paul.novack@gmail.com>
2025-10-17 08:26:35 -0500

    asr down

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
 <body>
 <div id="app" class="p-0 m-0 w-screen h-screen">
     <h1>WebRTC end-of-speech + Reverb + Faster-Whisper</h1>
-
+    <h1>The ASR Engine and Speech engine are down you can see a demo video in my linked in posts</h1>
     <div style="margin-bottom: 12px">
         <span class="chip">Session: @{{ sessionId }}</span>
         <button @click="init" :disabled="inited">Init Mic</button>
