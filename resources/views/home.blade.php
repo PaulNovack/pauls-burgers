ab7508061bb3dfc51631bb4d4452cf1f3ca300eb
Paul Novack <paul.novack@gmail.com>
2025-10-17 08:28:33 -0500

    down

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,9 @@
     </style>
 </head>
 <body>
+<div id="down">
+    <h1>The ASR Engine and Speech engine are down you can see a demo video in my linked in posts</h1>
+</div>
 <div id="app" class="p-0 m-0 w-screen h-screen">
     <h1>WebRTC end-of-speech + Reverb + Faster-Whisper</h1>
     <h1>The ASR Engine and Speech engine are down you can see a demo video in my linked in posts</h1>
